65b247e5ea671b3a3095ff30f3317a3000b20d9e
Neelesh <neelesh@sedintechnologies.com>
2024-02-13 23:43:13 +0530

    Refactor Azure TTS code and update UI

--- pages/azure_tts.py
+++ pages/azure_tts.py
@@ -11,13 +11,12 @@ st.set_page_config(page_title="Azure TTS", page_icon="🔊", layout="wide")
 with open("static/style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
-speech_config = speechsdk.SpeechConfig(
-    subscription=os.environ.get("SPEECH_KEY"),
-    region=os.environ.get("SPEECH_REGION")
-)
-
 if not os.path.exists("temp"):
     os.makedirs("temp")
+
+speech_config = speechsdk.SpeechConfig(
+    subscription=os.environ.get("SPEECH_KEY"), region=os.environ.get("SPEECH_REGION")
+)
 
 file_config = speechsdk.audio.AudioOutputConfig(filename="temp/audio.wav")
 
@@ -27,7 +26,7 @@ def list_languages() -> list[str]:
     synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config,
         audio_config=file_config
-    )
+        )
     response = synthesizer.get_voices_async().get()
     locales = set([voice.locale for voice in response.voices])
     return sorted(locales)
@@ -38,7 +37,7 @@ def list_voices(locale: str) -> list[str]:
     synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config,
         audio_config=file_config
-    )
+        )
     result = synthesizer.get_voices_async(locale=locale).get()
     voices = sorted(result.voices, key=lambda voice: voice.short_name)
 
@@ -52,12 +51,17 @@ def text_to_wav(voice_name: str, text: str) -> bytes:
     synthesizer = speechsdk.SpeechSynthesizer(
         speech_config=speech_config,
         audio_config=audio_config
-    )
+        )
     result = synthesizer.speak_text_async(text).get()
     return result.audio_data
 
 
 st.title("Azure TTS")
+
+wavenet_description = """Microsoft Azure Text-to-Speech is a cloud-based service that allows developers to add natural-sounding speech synthesis to their applications. 
+It provides a simple and scalable way to generate high-quality speech from text, with a variety of voices and languages to choose from.
+"""
+# The service uses deep neural networks to generate speech that is expressive and realistic, making it ideal for a wide range of applications, including virtual assistants, audiobooks, and voiceovers for movies and TV shows.
 
 with st.sidebar:
     locales = list_languages()
@@ -66,20 +70,15 @@ with st.sidebar:
     language = st.selectbox("Select language", options=languages, index=languages.index("en"), key="language")
 
     locales2 = [l for l in locales if l.startswith(language)]
-    locale = st.selectbox("Select locale", options=locales2, index=locales2.index("en-US") if language=='en' else 0, key="locale")
+    locale = st.selectbox("Select locale", options=locales2, index=locales2.index("en-US") if language == "en" else 0, key="locale")
 
     voices = list_voices(locale)
 
-    gender = st.selectbox("Select gender", options=['All', 'Female', 'Male'], index=0, key="gender")
+    gender = st.selectbox("Select gender", options=["All", "Female", "Male"], index=0, key="gender")
     if gender != "All":
-        voices = [voice for voice in voices if voice.split(' | ')[-1] == gender]
+        voices = [voice for voice in voices if voice.split(" | ")[-1] == gender]
 
     voice_name = st.radio("Select voice", options=voices, index=0, key="voice_name")
-
-wavenet_description = """Microsoft Azure Text-to-Speech is a cloud-based service that allows developers to add natural-sounding speech synthesis to their applications. 
-It provides a simple and scalable way to generate high-quality speech from text, with a variety of voices and languages to choose from.
-"""
-# The service uses deep neural networks to generate speech that is expressive and realistic, making it ideal for a wide range of applications, including virtual assistants, audiobooks, and voiceovers for movies and TV shows.
 
 text = st.text_area("Text to synthesize", value=wavenet_description, height=400)
 
